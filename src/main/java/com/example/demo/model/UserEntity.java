0e3ff031e58e0c74ecfba62687ca2891d9f31428
bala-amypo <bala@amypo.in>
2025-12-18 04:44:01 +0000

    User commit

--- src/main/java/com/example/demo/model/UserEntity.java
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -1,4 +1,4 @@
-package com.example.demo.entity;
+ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,37 +12,62 @@ public class LocationEntity {
     private Long id;
     private String fullName;
     private String email;
-    private ;
+    private String password;
+    private String role="USER";
+    private LocalDateTime createdAt=LocalDateTime.now();
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
-    public Double getLatitude() {
-        return latitude;
+    public String getEmail() {
+        return email;
     }
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public Double getLongitude() {
-        return longitude;
+    public String getPassword() {
+        return password;
     }
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
     public LocationEntity() {
     }
-    public LocationEntity(Long id, String name, Double latitude, Double longitude) {
+    public LocationEntity(Long id, String fullName, String email, String password, String role,
+            LocalDateTime createdAt) {
         this.id = id;
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
     }
-}
+    
+    
+    
+    }
+     
+    
+
+
+    
