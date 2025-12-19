67123a6c4162e69d4a6a29b6ed3a423d6ce0f574
bala-amypo <bala@amypo.in>
2025-12-19 17:53:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User register(@Valid @RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAllUsers();
+    }
+}
