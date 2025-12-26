993171b27b04947e32d61ac78c9b47ffcc6f15c2
bala-amypo <bala@amypo.in>
2025-12-26 08:51:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -1,7 +1,10 @@
  package com.example.demo.repository;
 
 import com.example.demo.entity.CategorizationRule;
+import com.example.demo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
+    List<CategorizationRule> findByCategoryOrderByPriorityAsc(Category category);
 }
--- src/main/java/com/example/demo/repository/VendorRepository.java
+++ src/main/java/com/example/demo/repository/VendorRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
-    Optional<Vendor> findByName(String name);
+    Optional<Vendor> findByVendorName(String vendorName);
 }
