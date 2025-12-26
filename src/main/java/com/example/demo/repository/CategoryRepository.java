e970dc79106a79cf29dd2f1923485f14d2eb3a32
bala-amypo <bala@amypo.in>
2025-12-26 08:56:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -2,8 +2,6 @@
 
 import com.example.demo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByCategoryName(String categoryName);
 }
--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,10 +1,10 @@
  package com.example.demo.repository;
 
 import com.example.demo.entity.Invoice;
-import com.example.demo.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    Optional<Invoice> findByVendorAndInvoiceNumber(Vendor vendor, String invoiceNumber);
+    Optional<Invoice> findByInvoiceNumberAndVendorId(String invoiceNumber, Long vendorId);
 }
